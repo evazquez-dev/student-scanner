6615ca55d56681e19d1bebe8a4d1acd606f22a98
Erick Vazquez <erickvazquez@Ericks-MacBook-Pro.local>
2026-01-06 09:29:28 -0500

    version bump

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = 'v16-2025-12-15'; // bump on each deploy
+const VERSION = 'v17-2026-01-06'; // bump on each deploy
 const STATIC_CACHE = `static-${VERSION}`;
 
 const PRECACHE = [
